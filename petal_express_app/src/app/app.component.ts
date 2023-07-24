ca055da89968836f598c87be4eb9934860723d92
Simul <simulbista@gmail.com>
2023-07-24 19:26:50 -0400

    added conditional display for footer for login, added server url env variable

--- petal_express_app/src/app/app.component.ts
+++ petal_express_app/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  constructor(private router: Router) {}
   title = 'petal_express_app';
+  isLoginRoute(): boolean {
+    // Check if the current route is the login route
+    return this.router.url === '/login';
+  }
 }
