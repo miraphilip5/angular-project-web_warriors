78caee8e902932e9696699f4189be2232f9a7a53
Simul <simulbista@gmail.com>
2023-07-23 23:33:46 -0400

    added navigation routes in app template

--- petal_express_app/src/app/app-routing.module.ts
+++ petal_express_app/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { CartComponent } from './components/cart/cart.component';
+import { OrdersComponent } from './components/orders/orders.component';
+import { FlowersComponent } from './components/flowers/flowers.component';
 
-const routes: Routes = [];
+const routes: Routes = [                                      
+  {path: 'home' , component:FlowersComponent},
+  {path: 'cart' , component: CartComponent},
+  {path: 'orders' , component: OrdersComponent},
+  {path: 'login' , component: LoginComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
