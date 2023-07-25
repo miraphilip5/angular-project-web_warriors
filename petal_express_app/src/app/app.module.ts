728fe5ba0e23c5806d8970ec0f0309f6f561d84c
Simul <simulbista@gmail.com>
2023-07-24 22:37:17 -0400

    login and registration - both working in terms of functionality, both use template driven forms, need to convert registration to template driven form

--- petal_express_app/src/app/app.module.ts
+++ petal_express_app/src/app/app.module.ts
@@ -1,18 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FooterComponent
+    FooterComponent,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- petal_express_app/src/app/components/register/register.component.ts
+++ petal_express_app/src/app/components/register/register.component.ts
@@ -1,13 +1,66 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { FormsModule } from '@angular/forms'; 
+import { environment } from '../../../enviroments/enviroments';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent {
+  formData = {
+    name: '',
+    email: '',
+    password: '',
+    password2: '',
+  };
+  validationError = '';
 
+  constructor(private router: Router, private http: HttpClient) {}
+
+  onSubmit() {
+    this.validationError = '';
+
+    const { name, email, password, password2 } = this.formData;
+
+    if (password && password.length < 6) {
+      this.validationError = 'Password should be at least 6 characters long!';
+      return;
+    } else if (password2 !== password) {
+      this.validationError = 'Passwords must match!';
+      return;
+    }
+
+    const data = {
+      name,
+      email,
+      password
+    };
+
+    const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
+    const options = { headers };
+
+    //calling login backend api i.e. http://localhost:8080/api/user
+    const serverUrl = `${environment.serverUrl}/api/user`;
+
+    this.http.post(serverUrl, data, options).subscribe(
+      (response: any) => {
+        console.log(response);
+        localStorage.setItem('token', response.token);
+        localStorage.setItem('isJustRegistered', 'true');
+        this.router.navigate(['/login']);
+      },
+      (error) => {
+        if (error && error.error && error.error.length) {
+          this.validationError = error.error[0].msg;
+        }
+        console.log('error ', error);
+      }
+    );
+  }
 }
