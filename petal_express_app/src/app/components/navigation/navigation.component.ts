0874a930a2cf2b8ae506e93ae6d4997e1c6c2a08
miraphilip5 <miraphilip9@gmail.com>
2023-07-31 21:08:01 -0400

    add header footer html css

--- petal_express_app/src/app/components/navigation/navigation.component.ts
+++ petal_express_app/src/app/components/navigation/navigation.component.ts
@@ -1,14 +1,22 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { Router } from '@angular/router';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-navigation',
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, MatToolbarModule],
   templateUrl: './navigation.component.html',
   styleUrls: ['./navigation.component.css']
 })
 export class NavigationComponent {
+  constructor(private router: Router) {}
 
+  handleLogout() {
+    // Perform logout logic and remove the token from local storage
+    localStorage.removeItem('token');
+    this.router.navigate(['/login']);
+  }
 }
